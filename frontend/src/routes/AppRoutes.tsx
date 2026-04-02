16dde8f4c65de2bc3829069d758849c764061114
Aayush18012000 <kanyal.aayush.18et1022@gmail.com>
2026-04-02 14:26:20 +0530

    Created Navbar and created some pages

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Services from "../pages/Services";
+import Navbar from "../components/Navbar";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
